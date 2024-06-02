0b7319e2ca51a3927073540e49f0f93e13ec1afe
Selly <selly.gazzola@gmail.com>
2024-06-02 19:12:57 +0200

    1st commit

--- lib/navigation/navtools.dart
+++ lib/navigation/navtools.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:simfit/screens/login.dart';
 import 'package:simfit/screens/profile.dart';
+import 'package:profile_photo/profile_photo.dart';
 
 class NavDrawer extends StatelessWidget {
   NavDrawer({Key? key}) : super(key: key);
@@ -10,23 +12,73 @@ class NavDrawer extends StatelessWidget {
   Widget build(BuildContext context) {
     return Drawer(
       child: ListView(
-        children: [
-          ListTile(
-            title: Text('Profile Page'),
-            onTap: () {
-              Navigator.pop(context);
-              _toProfilePage(context);
-            },
+        padding: EdgeInsets.zero,
+        children: <Widget>[
+          DrawerHeader(
+            decoration: BoxDecoration(
+            color: Colors.blue,
+            ), // BoxDecoration
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children:[
+                Text(
+                  'Hello User!',
+                  style: TextStyle(
+                    fontWeight:FontWeight.bold,
+                    fontSize: 15,
+                    color: Colors.white,
+                  ),
+                  textAlign: TextAlign.left,
+                ),
+                ProfilePhoto(
+                  totalWidth: 80,
+                  cornerRadius: 360,
+                  color: Colors.white,
+                  image: const AssetImage('assets/user_avatar.jpg'),
+                ),
+              ]
+            ),
           ),
           ListTile(
-            title: Text('LOG OUT'),
-            onTap: () {
-              Navigator.pop(context);
-              _toLoginPage(context);
-            },
-          )
-        ],
-      ),
+            trailing: const Icon(Icons.account_circle),
+            title: const Text('Profile'),
+            onTap: () {_toProfilePage(context);}
+          ),
+          ListTile(
+            trailing: const Icon(Icons.settings),
+            title: const Text('Settings'),
+            onTap: () {_toSettingPage(context); }
+          ),
+          ListTile(
+            trailing: const Icon(Icons.info),
+            title: const Text('Info'),
+            onTap: () {_toInfoPage(context);}
+          ),
+          Padding(
+              padding: EdgeInsets.only(left:60, right:60, top:370),
+              child: ElevatedButton(
+                onPressed: () async {
+                  _toLoginPage(context);
+                },
+                style: ButtonStyle(
+                    padding: MaterialStateProperty.all<EdgeInsetsGeometry>(
+                        const EdgeInsets.symmetric(
+                            horizontal: 60, vertical: 10)),
+                    backgroundColor:
+                        MaterialStateProperty.all<Color>(Colors.blue),
+                ),
+                child: Text('Log out',
+                  style: TextStyle(
+                    fontWeight:FontWeight.bold,
+                    fontSize: 13,
+                    color: Colors.white,
+                  ),
+                  textAlign: TextAlign.left,
+                ),
+              ),
+            ),
+        ] 
+      )
     );
   }
 
@@ -43,3 +95,13 @@ class NavDrawer extends StatelessWidget {
         MaterialPageRoute(builder: ((context) => const Login())));
   }
 }
+
+void _toInfoPage(BuildContext context) {
+    Navigator.of(context)
+        .push(MaterialPageRoute(builder: ((context) => const Info())));
+  }
+
+void _toSettingPage(BuildContext context) {
+    Navigator.of(context)
+        .push(MaterialPageRoute(builder: ((context) => const Settings())));
+  }
---
+++ lib/screens/settings.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class Settings extends StatelessWidget {
+  const Settings({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Info Page'),
+        centerTitle: true,
+        leading: IconButton(
+          icon: Icon(Icons.arrow_back_sharp),
+          onPressed: () {
+            Navigator.pop(context);
+          },
+        ),
+      ),
+    );
+  }
+}
