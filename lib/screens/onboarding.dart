0518b0e7091588f1c87545a5b09d71a613595b36
Alessandro Casarin <alessandrocasarin27@gmail.com>
2024-07-13 14:22:15 +0200

    graphic onboarding

--- lib/screens/onboarding.dart
+++ lib/screens/onboarding.dart
@@ -10,169 +10,178 @@ class OnBoarding extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return OnBoardingSlider(
-      finishButtonText: 'Login',
-      onFinish: () async {
-        await Provider.of<UserProvider>(context, listen: false).onboardUser();
-        Navigator.of(context).pushReplacement(
-            MaterialPageRoute(builder: ((context) => const Login())));
-      },
-      finishButtonStyle: FinishButtonStyle(
-        backgroundColor: Theme.of(context).primaryColor,
-      ),
-      skipTextButton: Text(
-        'Skip',
-        style: TextStyle(
-          fontSize: 20,
-          color: Theme.of(context).primaryColor,
-          fontWeight: FontWeight.w600,
-        ),
-      ),
-      trailing: Text(
-        'Learn more',
-        style: TextStyle(
-          fontSize: 20,
-          color: Theme.of(context).primaryColor,
-          fontWeight: FontWeight.w600,
-        ),
-      ),
-      trailingFunction: () {
-        Navigator.of(context)
-            .push(MaterialPageRoute(builder: ((context) => const Info())));
-      },
-      controllerColor: Theme.of(context).primaryColor,
-      totalPage: 3,
-      headerBackgroundColor: Colors.white,
-      pageBackgroundColor: Colors.white,
-      centerBackground: true,
-      background: [
-        Padding(
-          padding: EdgeInsets.all(10),
-          child:Image.asset(
-          'assets/allenamento1.png',
-          height: 400,
-        ),),
-        Padding(
-          padding: EdgeInsets.all(10),
-          child:Image.asset(
-          'assets/grafico2.jpg',
-          height: 400,
-        ),),
-        Padding(
-          padding: EdgeInsets.all(10),
-          child:Image.asset(
-          'assets/ciclista.jpg',
-          height: 400,
-        ),),
-      ],
-      speed: 1.8,
-      pageBodies: [
-        Container(
-          alignment: Alignment.center,
-          width: MediaQuery.of(context).size.width,
-          padding: const EdgeInsets.symmetric(horizontal: 40),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: <Widget>[
-              const SizedBox(
-                height: 480,
-              ),
-              Text(
-                'Track',
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                  color:  Theme.of(context).primaryColor,
-                  fontSize: 30.0,
-                  fontWeight: FontWeight.w600,
-                ),
-              ),
-              const SizedBox(
-                height: 20,
-              ),
-              const Text(
-                'Keep track of your training data collected by your Fitbit watch',
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                  color: Colors.black,
-                  fontSize: 24.0,
-                  fontWeight: FontWeight.w600,
-                ),
-              ),
-            ],
+    return SafeArea(
+      child: OnBoardingSlider(
+        finishButtonText: 'LOG IN',
+        onFinish: () async {
+          await Provider.of<UserProvider>(context, listen: false).onboardUser();
+          Navigator.of(context).pushReplacement(
+              MaterialPageRoute(builder: ((context) => const Login())));
+        },
+        finishButtonStyle: FinishButtonStyle(
+          backgroundColor: Theme.of(context).primaryColor,
+          foregroundColor: Theme.of(context).secondaryHeaderColor,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(20),
           ),
         ),
-        Container(
-          alignment: Alignment.center,
-          width: MediaQuery.of(context).size.width,
-          padding: const EdgeInsets.symmetric(horizontal: 40),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: <Widget>[
-              const SizedBox(
-                height: 480,
-              ),
-              Text(
-                'Simulate',
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                  color:  Theme.of(context).primaryColor,
-                  fontSize: 30.0,
-                  fontWeight: FontWeight.w600,
-                ),
-              ),
-              const SizedBox(
-                height: 20,
-              ),
-              const Text(
-                'Simulate the training loads according to your features and objectives',
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                  color: Colors.black,
-                  fontSize: 24.0,
-                  fontWeight: FontWeight.w600,
-                ),
-              ),
-            ],
+        skipTextButton: Text(
+          'Skip',
+          style: TextStyle(
+            fontSize: 20,
+            color: Theme.of(context).primaryColor,
+            fontWeight: FontWeight.w600,
           ),
         ),
-        Container(
-          alignment: Alignment.center,
-          width: MediaQuery.of(context).size.width,
-          padding: const EdgeInsets.symmetric(horizontal: 40),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: <Widget>[
-              const SizedBox(
-                height: 480,
-              ),
-              Text(
-                'Train',
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                  color:  Theme.of(context).primaryColor,
-                  fontSize: 30.0,
-                  fontWeight: FontWeight.w600,
-                ),
-              ),
-              const SizedBox(
-                height: 20,
-              ),
-              const Text(
-                'Tune the progress of your performance and start!',
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                  color: Colors.black,
-                  fontSize: 24.0,
-                  fontWeight: FontWeight.w600,
-                ),
-              ),
-            ],
+        trailing: Text(
+          'Learn more',
+          style: TextStyle(
+            fontSize: 20,
+            color: Theme.of(context).primaryColor,
+            fontWeight: FontWeight.w600,
           ),
         ),
-      ],
+        trailingFunction: () {
+          Navigator.of(context)
+              .push(MaterialPageRoute(builder: ((context) => const Info())));
+        },
+        controllerColor: Theme.of(context).primaryColor,
+        totalPage: 3,
+        headerBackgroundColor: Colors.white,
+        pageBackgroundColor: Colors.white,
+        centerBackground: true,
+        background: [
+          Padding(
+            padding: EdgeInsets.all(10),
+            child: Image.asset(
+              'assets/allenamento1.png',
+              height: 400,
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.all(10),
+            child: Image.asset(
+              'assets/grafico2.jpg',
+              height: 400,
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.all(10),
+            child: Image.asset(
+              'assets/ciclista.jpg',
+              height: 400,
+            ),
+          ),
+        ],
+        speed: 1.8,
+        pageBodies: [
+          Container(
+            alignment: Alignment.center,
+            width: MediaQuery.of(context).size.width * 0.9,
+            padding: const EdgeInsets.symmetric(horizontal: 40),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+                const SizedBox(
+                  height: 420,
+                ),
+                Text(
+                  'Track',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    color: Theme.of(context).primaryColor,
+                    fontSize: 30.0,
+                    fontWeight: FontWeight.w600,
+                  ),
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                const Text(
+                  'Keep track of your training data collected by your Fitbit watch',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 24.0,
+                    fontWeight: FontWeight.w600,
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Container(
+            alignment: Alignment.center,
+            width: MediaQuery.of(context).size.width * 0.9,
+            padding: const EdgeInsets.symmetric(horizontal: 40),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+                const SizedBox(
+                  height: 420,
+                ),
+                Text(
+                  'Simulate',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    color: Theme.of(context).primaryColor,
+                    fontSize: 30.0,
+                    fontWeight: FontWeight.w600,
+                  ),
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                const Text(
+                  'Visualize your training load and simulate training sessions that suits your objectives',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 22.0,
+                    fontWeight: FontWeight.w600,
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Container(
+            alignment: Alignment.center,
+            width: MediaQuery.of(context).size.width * 0.9,
+            padding: const EdgeInsets.symmetric(horizontal: 40),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+                const SizedBox(
+                  height: 420,
+                ),
+                Text(
+                  'Train',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    color: Theme.of(context).primaryColor,
+                    fontSize: 30.0,
+                    fontWeight: FontWeight.w600,
+                  ),
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                const Text(
+                  'Tune the progress of your performance and start!',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 24.0,
+                    fontWeight: FontWeight.w600,
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
